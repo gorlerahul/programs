1f9e834d6a2f651df343e432eff8c035ecd16cc9
gorlerahul <166210055+gorlerahul@users.noreply.github.com>
2024-08-12 15:56:17 +0530

    Add files via upload

---
+++ amoperation.py
@@ -0,0 +1,12 @@
+a=int(input())
+b=int(input())
+c=a+b
+print('sun of {} and {} is:{}'.format(a,b,c))
+print('subraction of {} and {} is:{}'.format(a,b,a-b))
+print('multiplication of {} and {} is:{}'.format(a,b,a*b))
+print('division of {} and {} is:{}'.format(a,b,a/b))
+print('floored quotient of {} and {} is:{}'.format(a,b,a//b))
+print('remainder of {} and {} is:{}'.format(a,b,a%b))
+print('power of {} and {} is:{}'.format(a,b,a**b))
+
+
